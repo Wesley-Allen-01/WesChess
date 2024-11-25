bdae543fa1c7294c900f8a526a1cfc0c837eaa79
Wesley Allen <weal2937@colorado.edu>
2024-11-25 09:42:31 -0700

    added routes for user register and login

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"WesChess/internal/db"
+	"WesChess/internal/handlers"
 	"log"
 	"github.com/gin-gonic/gin"
 )
@@ -17,6 +18,9 @@ func main() {
 			"message": "Hello, World!",
 		})
 	})
+
+	router.POST("/register", handlers.RegisterHandler(db.DB))
+	router.POST("/login", handlers.LoginHandler(db.DB))
 	log.Println("Server started on :8080")
 
 	router.Run(":8080")
